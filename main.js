9fd779d1fafb69134dcbee563246f65501aa4d96
Fonzie <mickfilipe@hotmail.com>
2015-05-05 22:32:14 +0100

    a0.4.3
    -Improvements to background.js;
    -reverted to a0.4.1 viewport settings

--- main.js
+++ main.js
@@ -580,46 +580,33 @@ function sprite(image, imageSize, frameWidth, frameHeight, startFrame, endFrame,
 			this.imageSize, this.imageSize);
 	};
 }
-function background(speed, image, section) {
-	particle.call(this, speed);
+function background(section) {
 
-	this.direction = Math.PI/2;
+	this.speed = 180*dt;
 	this.section = section;
 	this.width = game.width;
 	this.height = (game.width * (16 / 9)) * 4 ;
 	this.x = 0;
 	this.y = (this.section === 0) ? -(this.height-game.height) : this.y = -(this.height);
 	this.image = 'level' + game.level + '.jpg'; //needs to be consecutive for this to work
-	this.limits = -game.height*0.02;
+	this.limits = -game.height*0.01; // *0.01 to disguise my bad image manipulation skills
 
 	this.update = function() {
-		this.vx = Math.cos(this.direction) * (speed*dt);
-		this.vy = Math.sin(this.direction) * (speed*dt);
-		// this.handleSprings();
-		// this.handleGravitations();
-		// this.vx *= this.friction;
-		// this.vy *= this.friction;
-		// this.vy += this.gravity;
-		this.x += this.vx;
-		this.y += this.vy;
-	};
-
-
-	this.draw = function() {
+		this.y += this.speed;
 		game.contextBackground.drawImage(game.images[this.image], this.x, this.y, this.width, this.height);
 	};
 
 	this.load = function() {
 		if (game.background.length < 1)
 		{					
-			game.background.push(new background(150, this.image, 0));			
+			game.background.push(new background(0));			
 		}
 
 		for (var b in game.background)
 		{
 			if (game.background[b].y > this.limits && game.background.length < 2){
 
-				game.background.push(new background(150, this.image, 1));				
+				game.background.push(new background(1));				
 			}
 
 			if (game.background[b].y > game.height){
@@ -627,13 +614,9 @@ function background(speed, image, section) {
 			}
 
 			game.background[b].update();			
-			game.background[b].draw();				
 		}
 	};
 }
-
-background.prototype = Object.create(particle.prototype); // Creating a background.prototype object that inherits from particle.prototype.
-background.prototype.constructor = background; // Set the "constructor" property to refer to background
 
 gameBackground = new background();
 function explosion(x, y, speed, direction, size) {
